88412a5a3a22523fc6fffdea8c0c861a2551554d
Matteo0421 <matteo.carola99@gmail.com>
2024-04-08 16:30:43 +0200

    [add]first-array

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -39,6 +39,6 @@ body::before {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.3); /* Imposta un colore di sfondo semi-trasparente */
+  background-color: rgba(0, 0, 0, 0.3); 
 }
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -2,5 +2,31 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-  counter: 0
+  CardList: [
+    {
+      img:'../img/h5-custom-icon-1.png' ,
+      title: 'Languages',
+    },
+    {
+      img:'../img/h5-custom-icon-2.png' ,
+      title: 'Software',
+    },
+    {
+      img:'../img/h5-custom-icon-3.png' ,
+      title:'Business' ,
+    },
+    {
+      img:'../img/h5-custom-icon-4.png' ,
+      title: 'Chemistry',
+    },
+    {
+      img:'../img/h5-custom-icon-5.png' ,
+      title:'Science' ,
+    },
+    {
+      img:'../img/h5-custom-icon-6.png' ,
+      title:'DIY&Craft' ,
+    },
+    
+  ]
 })
