ef00564d1865cc76706abbcea5d0216456358185
Matteo0421 <matteo.carola99@gmail.com>
2024-04-09 09:30:35 +0200

    [add]first-carosel

--- src/components/partials/BlueCarosel.vue
+++ src/components/partials/BlueCarosel.vue
@@ -1,20 +1,68 @@
 <script>
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  // Import Swiper styles
+  import 'swiper/css';
+
+  import 'swiper/css/pagination';
+
+
+  // import required modules
+  import { Pagination } from 'swiper/modules';
+
   export default {
-    
-  }
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      return {
+        modules: [Pagination],
+      };
+    },
+  };
 </script>
 
 <template>
-  <div>
-
-  </div>
+  <swiper
+    :pagination="{
+      dynamicBullets: true,
+    }"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide>
+      <div class="slide-carosel d-flex flex-column justify-content-center align-items-center">
+      <img src="../../img/img-secondarie/testimonials-standard-2.png" alt="2" class="mb-4">
+      <p class="mb-4 text-white ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo praesentium ea dolore esse dicta recusandae nemo fuga mollitia sint nulla incidunt quod ipsam ab omnis eum soluta nostrum doloribus, unde consequatur quam repellat accusamus. Corporis quibusdam voluptatem praesentium amet odio omnis eveniet temporibus reprehenderit, mollitia tenetur magnam voluptas fugiat quos.</p>
+      <h5 class="mb-3 text-white">Joan Collins</h5>
+      <span class="text-white">STUDENT</span>
+    </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="slide-carosel d-flex flex-column justify-content-center align-items-center">
+      <img src="../../img/img-secondarie/testimonials-standard-2.png" alt="2" class="mb-4">
+      <p class="mb-4 text-white ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo praesentium ea dolore esse dicta recusandae nemo fuga mollitia sint nulla incidunt quod ipsam ab omnis eum soluta nostrum doloribus, unde consequatur quam repellat accusamus. Corporis quibusdam voluptatem praesentium amet odio omnis eveniet temporibus reprehenderit, mollitia tenetur magnam voluptas fugiat quos.</p>
+      <h5 class="mb-3 text-white">Joan Collins</h5>
+      <span class="text-white">STUDENT</span>
+    </div>
+    </swiper-slide>
+  </swiper>
 </template>
+
+
 
 
 
 <style lang="scss" scoped>
 div{
   background-image: url(../../img/img-secondarie/h5-parallax-img-1.png);
-  height: 600px
+  height: 600px;
+}.slide-carosel{
+  width: 70%;
+  margin: 0 auto;
+}img{
+  width: 120px;
 }
 </style>
