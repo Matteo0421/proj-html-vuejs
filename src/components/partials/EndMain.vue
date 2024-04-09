e0c766bda1ba12f787ce371c108b08a0ffd7ae62
Matteo0421 <matteo.carola99@gmail.com>
2024-04-09 10:22:58 +0200

    [end]section

--- src/components/partials/EndMain.vue
+++ src/components/partials/EndMain.vue
@@ -18,8 +18,18 @@
     </div>
   </div>
   <div class="col-8">
-    <div class="tab-content" id="nav-tabContent">
-      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">1.</div>
+    <div class="tab-content position-relative " id="nav-tabContent">
+      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+        <h2>Learning Possibilites</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure vero totam sit dolorum dolore. Enim rem, sapiente, odit, cum pariatur accusamus tenetur omnis molestiae sunt provident ratione ullam nemo adipisci sit minus molestias veritatis iste?</p>
+        <ul class="list-unstyled">
+          <li><i class="fa-solid fa-check" style="color: #39c3ff;"></i> We enrich through learning.</li>
+          <li><i class="fa-solid fa-check" style="color: #39c3ff;"></i> Maximizing potential through individual attention.</li>
+          <li><i class="fa-solid fa-check" style="color: #39c3ff;"></i> The trusted name for specialized training.</li>
+          <li><i class="fa-solid fa-check" style="color: #39c3ff;"></i> People teach. People learn. This is where they connect.</li>
+        </ul>
+        <img src="../../img/img-secondarie/h12-tabs-icon-1.png" alt="">
+      </div>
     </div>
   </div>
 </div>
@@ -36,5 +46,18 @@
 }.my-active{
   color: #36C3FF;
   border-left: 10px solid #36C3FF;
+}h2{
+  margin-top: 40px;
+}p{
+  color: #6B5E5E;
+  margin-bottom: 120px;
+  margin-top: 40px;
+}li{
+  color: #6B5E5E;
+  margin-bottom: 30px;
+}img{
+  position: absolute;
+  bottom: -25px;
+  right: 0;
 }
 </style>
